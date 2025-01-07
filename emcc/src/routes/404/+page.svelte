7da9d2e3f85da3569eff6d0fa3295be296663513
lights0123 <developer@lights0123.com>
2025-01-07 10:08:14 -0500

    add 404

---
+++ emcc/src/routes/404/+page.svelte
@@ -0,0 +1,4 @@
+<div class="h-screen flex flex-col items-center justify-center">
+	<h1 class="text-3xl">Page not Found</h1>
+	<a href="/" class="underline text-blue-700">To the main page</a>
+</div>
